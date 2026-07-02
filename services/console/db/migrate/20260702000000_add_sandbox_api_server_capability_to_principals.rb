00818707588c036f7a7f509f25e60136b1504587
Matthew Slipper <me@matthewslipper.com>
2026-07-02 15:52:37 -0600

    fix: harden sandbox API capability rollout

--- services/console/db/migrate/20260702000000_add_sandbox_api_server_capability_to_principals.rb
+++ services/console/db/migrate/20260702000000_add_sandbox_api_server_capability_to_principals.rb
@@ -1,4 +1,4 @@
-class AddSandboxApiServerCapabilityToPrincipals < ActiveRecord::Migration[8.0]
+class AddSandboxApiServerCapabilityToPrincipals < ActiveRecord::Migration[8.1]
   def change
     add_column :principals, :sandbox_api_server_enabled, :boolean, null: false, default: true
   end
