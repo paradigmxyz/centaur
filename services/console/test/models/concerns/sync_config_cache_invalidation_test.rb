6793748b1ad943cfe5bed301969ee6dd23afbaff
Matthew Slipper <me@matthewslipper.com>
2026-07-29 11:37:56 -0700

    test: simplify sync config invalidation fixture

--- services/console/test/models/concerns/sync_config_cache_invalidation_test.rb
+++ services/console/test/models/concerns/sync_config_cache_invalidation_test.rb
@@ -3,29 +3,9 @@ require "test_helper"
 class SyncConfigCacheInvalidationTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
 
-  class TestRecord < ApplicationRecord
-    self.table_name = "roles"
-
-    include SyncConfigCacheInvalidation
-
-    attr_accessor :affected_principal_ids
-
-    private
-
-    def sync_config_affected_principal_ids
-      affected_principal_ids
-    end
-  end
-
-  def build_test_record(principal)
-    TestRecord.find(roles(:acme_infra).id).tap do |record|
-      record.affected_principal_ids = [ principal.id ]
-    end
-  end
-
   test "unchanged save does not enqueue a snapshot warm job" do
     principal = principals(:acme_channel)
-    record = build_test_record(principal)
+    record = static_secrets(:github_token_inject)
     version = principal.reload.sync_config_cache_version
     clear_enqueued_jobs
 
@@ -38,11 +18,11 @@ class SyncConfigCacheInvalidationTest < ActiveSupport::TestCase
 
   test "a later touch does not hide an earlier invalidating save in the same transaction" do
     principal = principals(:acme_channel)
-    record = build_test_record(principal)
+    record = static_secrets(:github_token_inject)
     version = principal.reload.sync_config_cache_version
 
-    TestRecord.transaction do
-      record.update!(name: "Updated")
+    StaticSecret.transaction do
+      record.update!(description: "Updated")
       record.touch
     end
 
