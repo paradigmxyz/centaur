eef8734eeb569d454c5f31c514af0bb02cfcd3eb
Matthew Slipper <me@matthewslipper.com>
2026-08-13 12:31:43 -0600

    fix: restore console user emails on rollback

--- services/console/db/migrate/20260813182623_remove_console_user_email_from_principals.rb
+++ services/console/db/migrate/20260813182623_remove_console_user_email_from_principals.rb
@@ -5,6 +5,12 @@ class RemoveConsoleUserEmailFromPrincipals < ActiveRecord::Migration[8.1]
 
   def down
     add_column :principals, :console_user_email, :string
+    execute <<~SQL
+      UPDATE principals
+      SET console_user_email = users.email
+      FROM users
+      WHERE principals.console_user_id = users.id
+    SQL
     add_index :principals, :console_user_email
   end
 end
